4347682823b680f4cf0ce1714145c8783b1f2fc0
nazarevitsch <bidaritter1997@gmail.com>
2021-11-22 00:49:50 +0200

    strong password validation, better only check that password contain any common passwords

--- src/main/java/com/bida/password/storage/validation/PasswordValidator.java
+++ src/main/java/com/bida/password/storage/validation/PasswordValidator.java
@@ -3,6 +3,8 @@ package com.bida.password.storage.validation;
 import com.bida.password.storage.exception.BadRequestException;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class PasswordValidator {
 
@@ -14,10 +16,20 @@ public class PasswordValidator {
     private static final int MIN_SPECIAL_CHARACTER_AMOUNT = 2;
     private static final int MIN_DIGIT_AMOUNT = 2;
 
+    private static final int MAX_AMOUNT_OF_SAME_CHARS = 2;
+
     private static final String KEYBOARD_PATTERN_NUMBERS = "01234567890";
+    private static final String KEYBOARD_PATTERN_NUMBERS_REVERSED = "09876543210";
+
     private static final String KEYBOARD_PATTERN_CHARACTER = "qwertyuiopasdfghjklzxcvbnm";
+    private static final String KEYBOARD_PATTERN_CHARACTER_REVERSED = "mnbvcxzlkjhgfdsapoiuytrewq";
+
+    private static final String ENGLISH_ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    private static final String ENGLISH_ALPHABET_REVERSED = "zyxwvutsrqponmlkjihgfedcba";
 
     private static final String ALLOWED_SPECIAL_CHARS = "/*!@#$%^&*(){}_[]|?<>,.";
+
+    private static final List<String> COMMON_WORDS = List.of("pass");
 
 
     public void validatePassword(String password, String email) {
@@ -33,20 +45,20 @@ public class PasswordValidator {
         int lowerLettersCount = 0;
         int specialCharacterCount = 0;
         int digitCount = 0;
-        for (int i = 0; i < passwordArray.length; i++) {
-            if (Character.isLowerCase(passwordArray[i])) {
+        for (char value : passwordArray) {
+            if (Character.isLowerCase(value)) {
                 lowerLettersCount++;
                 continue;
             }
-            if (Character.isUpperCase(passwordArray[i])) {
+            if (Character.isUpperCase(value)) {
                 upperLetterCount++;
                 continue;
             }
-            if (Character.isDigit(passwordArray[i])) {
+            if (Character.isDigit(value)) {
                 digitCount++;
                 continue;
             }
-            if (ALLOWED_SPECIAL_CHARS.contains(String.valueOf(passwordArray[i]))){
+            if (ALLOWED_SPECIAL_CHARS.contains(String.valueOf(value))) {
                 specialCharacterCount++;
                 continue;
             }
@@ -56,31 +68,51 @@ public class PasswordValidator {
         if (upperLetterCount < MIN_UPPER_CASE_LETTERS_AMOUNT) {
             throw new BadRequestException("Password has to contain at last " + MIN_UPPER_CASE_LETTERS_AMOUNT + " letters in upper case.");
         }
+
         if (lowerLettersCount < MIN_LOWER_CASE_LETTERS_AMOUNT) {
             throw new BadRequestException("Password has to contain at last " + MIN_LOWER_CASE_LETTERS_AMOUNT + " letters in lower case.");
         }
+
         if (digitCount < MIN_DIGIT_AMOUNT) {
             throw new BadRequestException("Password has to contain at last " + MIN_DIGIT_AMOUNT + " digit.");
         }
+
         if (specialCharacterCount < MIN_SPECIAL_CHARACTER_AMOUNT) {
             throw new BadRequestException("Password has to contain at last " + MIN_SPECIAL_CHARACTER_AMOUNT + " special characters.");
         }
 
+        String lowerCasePassword = password.toLowerCase();
         for (int i = 0; i < KEYBOARD_PATTERN_CHARACTER.length() - 3; i++) {
-            if (password.contains(KEYBOARD_PATTERN_CHARACTER.substring(i, i + 4))) {
+            if (lowerCasePassword.contains(KEYBOARD_PATTERN_CHARACTER.substring(i, i + 4))) {
                 throw new BadRequestException("Password can't contain keyboard sequence more then 3.");
             }
+            if (lowerCasePassword.contains(KEYBOARD_PATTERN_CHARACTER_REVERSED.substring(i, i + 4))) {
+                throw new BadRequestException("Password can't contain reversed keyboard sequence more then 3.");
+            }
         }
+
         for (int i = 0; i < KEYBOARD_PATTERN_NUMBERS.length() - 3; i++) {
             if (password.contains(KEYBOARD_PATTERN_NUMBERS.substring(i, i + 4))) {
                 throw new BadRequestException("Password can't contain keyboard sequence more then 3.");
             }
+            if (password.contains(KEYBOARD_PATTERN_NUMBERS_REVERSED.substring(i, i + 4))) {
+                throw new BadRequestException("Password can't contain reversed keyboard sequence more then 3.");
+            }
         }
 
-        String firstPartOfEmail = email.split("@")[0];
+        String firstPartOfEmail = email.split("@")[0].toLowerCase();
         for (int i = 0; i < firstPartOfEmail.length() - 3; i++) {
-            if (password.contains(firstPartOfEmail.substring(i, i + 4))) {
+            if (lowerCasePassword.contains(firstPartOfEmail.substring(i, i + 4))) {
                 throw new BadRequestException("Password should not contain parts of email.");
+            }
+        }
+
+        for (int i = 0; i < ENGLISH_ALPHABET.length() - 3; i++) {
+            if (lowerCasePassword.contains(ENGLISH_ALPHABET.substring(i, i + 4))) {
+                throw new BadRequestException("Password can't contain alphabet sequence more then 3.");
+            }
+            if (lowerCasePassword.contains(ENGLISH_ALPHABET_REVERSED.substring(i, i + 4))) {
+                throw new BadRequestException("Password can't contain reversed alphabet sequence more then 3.");
             }
         }
 
@@ -89,6 +121,24 @@ public class PasswordValidator {
             cutPassword = (password.substring(0, i) + password.substring(i + 4)).toLowerCase();
             if (cutPassword.contains(password.substring(i, i + 4).toLowerCase())) {
                 throw new BadRequestException("Password should not contain repeated parts.");
+            }
+        }
+
+        for (char c : passwordArray) {
+            int count = 0;
+            for (char letter : passwordArray) {
+                if (c == letter) {
+                    count++;
+                }
+            }
+            if (count > MAX_AMOUNT_OF_SAME_CHARS) {
+                throw new BadRequestException("Password can't contain more then 2 same character.");
+            }
+        }
+
+        for (String commonWord : COMMON_WORDS) {
+            if (lowerCasePassword.contains(commonWord)) {
+                throw new BadRequestException("Password can't contain: '" + commonWord + "', because it's common words, which people use to create a password.");
             }
         }
     }
